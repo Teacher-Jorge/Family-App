b8cc2927c8637234b2e0ba70df500f9755392958
Teacher-Jorge <79639664+Teacher-Jorge@users.noreply.github.com>
2022-12-18 20:34:13 -0300

    Add files via upload

--- page1.js
+++ page1.js
@@ -1,9 +1,4 @@
-let btnAugusto = document.getElementById('btnAugusto')
 let btnDaddy = document.getElementById('btnDaddy')
-
-btnAugusto.addEventListener('click', function() {
-    let audioAugusto = document.getElementById('audioAugusto').play()
-})
 
 btnDaddy.addEventListener('click', function() {
     let audioDaddy = document.getElementById('audioDaddy').play()
